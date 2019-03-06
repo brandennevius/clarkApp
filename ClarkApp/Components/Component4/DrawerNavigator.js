0eda0d86fda6957964d46b82e9ed5a820b108f22
Tim Powell <tpowell@clarku.edu>
2019-03-06 16:53:12 -0500

    Deleted comments

--- ClarkApp/Components/Component4/DrawerNavigator.js
+++ ClarkApp/Components/Component4/DrawerNavigator.js
@@ -11,10 +11,6 @@ const WIDTH = Dimensions.get('window').width;
 /*
 I watched this video to learn how to use the react-navigation DrawerNavigator
 https://www.youtube.com/watch?v=l3mw1e7rsuo
-Whenever you want to add another page to the drawer naviagtor...
-1)import the page 
-2)enter the name to be displayed and the name of the imported page
-ex. Settings: SettingsComponent,
 Whichever page is listed first is the first page that is displayed
 */ 
 
